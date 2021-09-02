9b9ee58816da37e331c2c901aba4e6abbdcd49b4
imhjlov <30456236+imhjlov@users.noreply.github.com>
2021-09-02 09:15:37 +0900

    chore: .eslintrc.ts 수정

--- .eslintrc.ts
+++ .eslintrc.ts
@@ -2,7 +2,7 @@ module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint', 'prettier'],
-  extends: ['standard-with-typescript', 'prettier', 'plugin:prettier/recommended'],
+  extends: ['standard-with-typescript', 'prettier', 'plugin:prettier/recommended', 'react-app'],
   parserOptions: {
     project: './tsconfig.json',
   },
