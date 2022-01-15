1319e0ffe563ef2c0f9eb5683f3369fae11aaa3d
imhjlov <30456236+imhjlov@users.noreply.github.com>
2022-01-15 11:53:55 +0900

    [#3] feat : axios를 이용해 todo check 수정 구현

---
+++ src/api/check.ts
@@ -0,0 +1,9 @@
+import { axiosCreate } from 'api/todo';
+import { END_POINT } from 'utils/constants';
+
+export const checkTodoAPI = async (id: number, isCheck: boolean) => {
+  const response = await axiosCreate.put(`${END_POINT}/${id}`, {
+    isCheck: isCheck,
+  });
+  return { msg: response.msg };
+};
