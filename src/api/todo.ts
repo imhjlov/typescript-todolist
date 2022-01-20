d4aa5c3e8a977785b45ceef248fc2aa9e765776f
imhjlov <30456236+imhjlov@users.noreply.github.com>
2022-01-20 18:41:46 +0900

    [#3] fix : todo 수정, 삭제 구분 안되는 오류 수정

--- src/api/todo.ts
+++ src/api/todo.ts
@@ -2,31 +2,42 @@ import axios from 'axios';
 import { TodoType } from 'types';
 import { END_POINT } from 'utils/constants';
 
+export interface IResponse {
+  msg: string;
+  status: number;
+  content?: string;
+}
+
 export const axiosCreate = axios.create({
   baseURL: `${process.env.REACT_APP_API_URL}`,
 });
 
 export const getTodoAll = async () => {
-  const response = await axiosCreate.get<TodoType[]>(`${END_POINT}`);
-  return { result: response.data };
+  const response = await axiosCreate.get<TodoType>(`${END_POINT}`);
+  return response;
 };
 
-export const addTodoAPI = async (todo: TodoType) => {
-  const response = await axiosCreate.post<TodoType[]>(`${END_POINT}`, {
-    ...todo,
+export const addTodoAPI = async (content: string) => {
+  const response = await axiosCreate.post<IResponse>(`${END_POINT}`, {
+    content,
   });
-  return { msg: response.data.msg };
+  return response;
 };
 
-export const updateTodoAPI = async (todo: TodoType) => {
-  const response = await axiosCreate.put<TodoType[]>(`${END_POINT}/${todo.id}`, {
-    id: todo.id,
-    content: todo.content,
-  });
-  return { msg: response.data.msg, result: response.content };
+export const updateTodoAPI = async (id: number, content: string) => {
+  const response = await axiosCreate.post<IResponse>(
+    `${END_POINT}/${id}`,
+    {
+      content: content,
+    },
+    { headers: { 'x-mock-match-request-body': 'true' } },
+  );
+  return response;
 };
 
 export const deleteTodoAPI = async (id: number) => {
-  const response = await axiosCreate.delete<TodoType[]>(`${END_POINT}/${id}`);
-  return { msg: response.data.msg, result: id };
+  const response = await axiosCreate.post<IResponse>(`${END_POINT}/${id}`, {
+    headers: { 'x-mock-match-request-body': 'true' },
+  });
+  return response;
 };
